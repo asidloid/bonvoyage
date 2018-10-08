3f82d9ad21b6fb5bf41ff73281d27d1591f430c3
aasidxxx <aasidxxxx@gmail.com>
2018-10-09 00:35:57 +0330

    routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,32 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route::get('/', function () {
+//    return view('index');
+//});
+Route::get('maket', 'IndexController@index');
+
+Route::get('k', 'KorsinaController@index');
+Route::get('categories', 'CategoryController@index');
+Route::get('categoriesid/{id}', 'CategoryController@id');
+
+Route::get('filtrtrek', 'FiltrController@filtrtrek');
+Route::get('filtr', 'FiltrController@index');
+Route::get('filter', 'FiltrController@vse');
+
+Route::get('gl', 'GlController@index');
+Route::get('gl2', 'GlController@index2');
+
+ Route::group(['prefix' => config('backpack.base.route_prefix'), 'middleware' => ['admin'], 'namespace' => 'Admin'], function()
+ {
+    CRUD::resource('menu', 'MenuCrudController');
+     CRUD::resource('podmenu', 'PodmenuCrudController');
+     CRUD::resource('galery', 'GaleryCrudController');
+     CRUD::resource('treck', 'TreckCrudController');
+     CRUD::resource('treklocation', 'TreklocationCrudController');
+     CRUD::resource('podtreck', 'PodtreckCrudController');
+     CRUD::resource('tovar', 'TovarCrudController');
+  CRUD::resource('predlog', 'PredlogCrudController');
+  CRUD::resource('news', 'NewsCrudController');
+ });
